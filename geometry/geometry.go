c35419e5f685cdbb83f6e8382ca1efcafeaee70f
Nguyen Thi Cam Li <linguyen@gmail.com>
2021-08-11 11:48:00 +0700

    Learn func init()/ Blank identifier

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -5,16 +5,30 @@ import (
 	"fmt"
 
 	"github.com/camli1803/Learn_GoLang/geometry/rectangle" //import package from github by module.
+
+	"log"
+	_ "math" //Blank identifier
 )
 
+var rectLen, rectWidth float64 = 6, 7
+
+func init() {
+	fmt.Println("main package initialized!")
+	if rectLen < 0 {
+		log.Fatal("Error: Lenght is less than 0")
+	}
+	if rectWidth < 0 {
+		log.Fatal("Error: Width is less than 0")
+	}
+
+}
 func main() {
-	var rectLen, rectWidth float64 = 6, 7
 	fmt.Println("Geometrical shape properties")
 
 	/*Hàm Area của package rectangle được sử dụng
 	 */
-	fmt.Printf("area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth))
+	fmt.Printf("Area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth))
 	/*Hàm Diagonal của package rectangle được sử dụng
 	 */
-	fmt.Printf("diagonal of the rectangle %.2f ", rectangle.Diagonal(rectLen, rectWidth))
+	fmt.Printf("Diagonal of the rectangle %.2f ", rectangle.Diagonal(rectLen, rectWidth))
 }
--- geometry/rectangle/rectprops.go
+++ geometry/rectangle/rectprops.go
@@ -1,9 +1,17 @@
 //rectprops.go
 package rectangle
 
-import "math"
+import (
+	"math"
 
-func Area(len, wid float64) float64 {
+	"fmt"
+)
+
+func init() {
+	fmt.Println("rectangle package initialized!")
+}
+
+func Area(len, wid float64) float64 { // viết hoa chữ cái đầu tiên thì mới có thể truy cập được từ bên ngoài pkg
 	area := len * wid
 	return area
 }
